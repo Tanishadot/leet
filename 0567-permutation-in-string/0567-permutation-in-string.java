97dd261aae5a2b3f8e0455c4bbb2fb3526c5a027
Tanisha D Jogi <jogitanisha@gmail.com>
2026-07-15 16:54:16 +0530

    Time: 23 ms (30.22%), Space: 46.9 MB (18.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -6,15 +6,12 @@ class Solution {
 
         HashMap<Character, Integer> s1Map = new HashMap<>();
         HashMap<Character, Integer> windowMap = new HashMap<>();
-
-        // Frequency map for s1
         for (char c : s1.toCharArray()) {
             s1Map.put(c, s1Map.getOrDefault(c, 0) + 1);
         }
 
         int windowSize = s1.length();
 
-        // Build the first window
         for (int i = 0; i < windowSize; i++) {
             char c = s2.charAt(i);
             windowMap.put(c, windowMap.getOrDefault(c, 0) + 1);
@@ -23,16 +20,14 @@ class Solution {
         if (s1Map.equals(windowMap)) {
             return true;
         }
-
-        // Slide the window
         for (int i = windowSize; i < s2.length(); i++) {
             char add = s2.charAt(i);
             char remove = s2.charAt(i - windowSize);
 
-            // Add new character
+            
             windowMap.put(add, windowMap.getOrDefault(add, 0) + 1);
 
-            // Remove old character
+
             windowMap.put(remove, windowMap.get(remove) - 1);
             if (windowMap.get(remove) == 0) {
                 windowMap.remove(remove);
